43e0dd1995c52801473efa7baca7ee67c2032f5d
Fizi <105849642+Fizina@users.noreply.github.com>
2023-05-08 22:22:26 +0330

    Add files via upload

---
+++ login.js
@@ -0,0 +1,29 @@
+function btnlogin(event){
+    event.preventDefault()
+  
+    //DataBase
+    let users = [
+        {id: 1, name: 'Bahar', lastname: 'Harivandi'},
+        {id: 2, name: 'joosi', lastname: 'joosi'},
+        {id: 3, name: 'bahar', lastname: 'harivandi'},
+        {id: 4, name: 'joosi', lastname: ''},
+        {id: 4, name: 'JOOSI', lastname: ''},
+        {id: 6, name: 'Joosi', lastname: ''},
+        {id: 7, name: 'BAHAR', lastname: 'HARIVANDI'},
+        {id: 8, name: 'JOOSI', lastname: 'JOOSI'},
+    ]
+let betDataFalse = false
+    //Bet Login
+    users.forEach(user => {
+        if (username.value == user.name && lastname.value == user.lastname) {
+            window.location.href = './Home/home.html' ;
+        }
+    else{betDataFalse = true}
+    })
+    
+    if(betDataFalse){
+        alert('You are not Joosi !')
+        username.value = ""
+        lastname.value = ""
+    }
+}
